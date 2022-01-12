626c4b4d59788d5edde736a719d2a2d667629ab1
Madhu Sivapragasam <madhuran.siva@gmail.com>
2022-01-11 22:11:46 -0500

    Setup project dependancies and connections

---
+++ config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI')
+
+//Attempt to connect to mongodb database, if error, exit process with failure
+const connectDB = async () => {
+    try {
+        await mongoose.connect(db)
+        console.log("MongoDB Connected...")
+    } catch(err) {
+        console.error(err.message)
+        process.exit(1)
+    }
+
+}
+
+module.exports = connectDB;

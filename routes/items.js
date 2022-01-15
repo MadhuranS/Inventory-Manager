ce44972ac49ec017b49bb314259fb97f6beb88b6
Madhu Sivapragasam <madhuran.siva@gmail.com>
2022-01-15 01:47:04 -0500

    add middleware validation

--- routes/items.js
+++ routes/items.js
@@ -12,32 +12,32 @@ router.post(
     "/",
     upload.single("image"),
     [
-        check("name", "Name must be a string").isString(),
-        check("name", "Name must be a string of at least length 1").notEmpty(),
+        check(
+            "name",
+            "Name must exist and be a string of at least length 1"
+        ).notEmpty(),
         check(
             "description",
-            "Description is required and must be a string"
-        ).isString(),
-        check(
-            "description",
-            "Description must be a string of at least length 1"
+            "Description must exist and be a string of at least length 1"
         ).notEmpty(),
         check(
             "quantity",
-            "quantity must be an integer value"
-        ).isInt()
+            "quantity must exist and be an integer value"
+        ).isInt(),
+        check(
+            "quantity",
+            "You must submit a quantity property with a positive integer value"
+        ).custom((value, { req }) => {
+            if (value < 0) {
+                return false;
+            }
+            return true;
+        }),
     ],
     async (req, res) => {
         try {
             //validate for correct inputs
             let errors = validationResult(req).array();
-            if (!("quantity" in req.body) || req.body.quantity < 0) {
-                errors.push({
-                    msg: "You must submit a quantity property with a positive integer value",
-                    param: "quantity",
-                    location: "body",
-                });
-            }
             if (
                 !req.hasOwnProperty("file") ||
                 !req.file.mimetype.startsWith("image/")
@@ -90,114 +90,134 @@ router.get("/", async (req, res) => {
     }
 });
 
-// @route PATCH api/items/:id
-// @desc Update an item
-router.patch("/:id", upload.single("image"), async (req, res) => {
+// @route GET api/items/:id
+// @desc Route to fetch one item
+router.get("/:id", async (req, res) => {
     try {
-        //checks for correct input values
-        let errors = [];
-        if (
-            "name" in req.body &&
-            (typeof req.body.name !== "string" || req.body.name.length < 1)
-        ) {
-            errors.push({
-                msg: "If name is passed, it must be a string of at least length 1",
-                param: "name",
-                location: "body",
-            });
-        }
-
-        if (
-            "description" in req.body &&
-            (typeof req.body.description !== "string" ||
-                req.body.description.length < 1)
-        ) {
-            errors.push({
-                msg: "If description is passed, it must be a string of at least length 1",
-                param: "description",
-                location: "body",
-            });
-        }
-
-        if (
-            "quantity" in req.body &&
-            (Number.isInteger(req.body.quantity) || req.body.quantity < 0)
-        ) {
-            errors.push({
-                msg: "If quantity is passed, it must be a positive integer",
-                param: "integer",
-                location: "body",
-            });
-        }
-
-        if (
-            req.hasOwnProperty("file") &&
-            !req.file.mimetype.startsWith("image/")
-        ) {
-            errors.push({
-                msg: "If an image is passed, it must be an image file ty[e",
-                param: "file",
-            });
-        }
-        if (errors.length > 0) return res.status(400).json({ errors: errors });
-
         //find item with given id
         const item = await Item.findById(req.params.id);
         if (!item) {
             return res.status(404).json({ msg: "Item not found" });
         }
 
-        //if file is passed, update the file value, otherwise ignore
-        let updatedItem = {};
-        if (req.hasOwnProperty("file")) {
-            //delete old image from cloudinary
-            if (item.thumbnail && item.thumbnail.public_id) {
-                const deleteErrors = await deleteImage(
-                    item.thumbnail.public_id
-                );
-                if (deleteErrors.length > 0) {
-                    return res.status(502).json({ errors: deleteErrors });
-                }
-            }
-
-            //upload new image to cloudinary and check if upload is successful
-            const uploadResponse = await uploadImage(req.file);
-            if (uploadResponse.errors.length > 0) {
-                return res.status(502).json({ errors: uploadResponse.errors });
-            }
-            updatedItem = {
-                name: req.body.name,
-                description: req.body.description,
-                thumbnail: {
-                    url: uploadResponse.data.url,
-                    public_id: uploadResponse.data.public_id,
-                },
-                quantity: req.body.quantity
-            };
-        } else {
-            updatedItem = {
-                name: req.body.name,
-                description: req.body.description,
-                quantity: req.body.quantity
-            };
-        }
-
-        //update item on mongodb
-        await item.updateOne(updatedItem);
-
-        //return json response
-        res.json({
-            msg: "Updated document",
-            bodyUpdates: req.body,
-            fileUpdates: req.file ? true : false,
-        });
+        //respond with item
+        res.json(item);
     } catch (err) {
-        //return error
-        console.error(err.message);
         console.error("Server error", err.message);
         res.status(500).send("Internal server error");
     }
 });
+
+// @route PATCH api/items/:id
+// @desc Update an item
+router.patch(
+    "/:id",
+    upload.single("image"),
+    [
+        check(
+            "name",
+            "If name is passed, it must be a string of at least length 1"
+        ).custom((value, { req }) => {
+            if (value != null && value.length < 1) {
+                return false;
+            }
+            return true;
+        }),
+        check(
+            "description",
+            "If description is passed, it must be a string of at least length 1"
+        ).custom((value, { req }) => {
+            if (value != null && value.length < 1) {
+                return false;
+            }
+            return true;
+        }),
+        check(
+            "quantity",
+            "If quantity is passed, it must be a positive integer"
+        ).custom((value, { req }) => {
+            if (value != null && (isNaN(value) || value < 0)) {
+                return false;
+            }
+            return true;
+        }),
+    ],
+    async (req, res) => {
+        try {
+            //checks for correct input values
+            let errors = validationResult(req).array();
+
+            if (
+                req.hasOwnProperty("file") &&
+                !req.file.mimetype.startsWith("image/")
+            ) {
+                errors.push({
+                    msg: "If an image is passed, it must be an image file ty[e",
+                    param: "file",
+                });
+            }
+            if (errors.length > 0)
+                return res.status(400).json({ errors: errors });
+
+            //find item with given id
+            const item = await Item.findById(req.params.id);
+            if (!item) {
+                return res.status(404).json({ msg: "Item not found" });
+            }
+
+            //if file is passed, update the file value, otherwise ignore
+            let updatedItem = {};
+            if (req.hasOwnProperty("file")) {
+                //delete old image from cloudinary
+                if (item.thumbnail && item.thumbnail.public_id) {
+                    const deleteErrors = await deleteImage(
+                        item.thumbnail.public_id
+                    );
+                    if (deleteErrors.length > 0) {
+                        return res.status(502).json({ errors: deleteErrors });
+                    }
+                }
+
+                //upload new image to cloudinary and check if upload is successful
+                const uploadResponse = await uploadImage(req.file);
+                if (uploadResponse.errors.length > 0) {
+                    return res
+                        .status(502)
+                        .json({ errors: uploadResponse.errors });
+                }
+                updatedItem = {
+                    name: req.body.name,
+                    description: req.body.description,
+                    thumbnail: {
+                        url: uploadResponse.data.url,
+                        public_id: uploadResponse.data.public_id,
+                    },
+                    quantity: req.body.quantity,
+                };
+            } else {
+                updatedItem = {
+                    name: req.body.name,
+                    description: req.body.description,
+                    quantity: req.body.quantity,
+                };
+            }
+
+            //update item on mongodb
+            await item.updateOne(updatedItem);
+
+            //return json response
+            res.json({
+                msg: "Updated document",
+                bodyUpdates: req.body,
+                fileUpdates: req.file ? true : false,
+            });
+        } catch (err) {
+            //return error
+            console.error(err.message);
+            res.status(500).send("Internal server error");
+        }
+    }
+);
 
 // @route DELETE api/items/:id
 // @desc Delete an item
